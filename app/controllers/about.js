7bff6f4627ac120799a767589a0c2b5ca6a278bf
Eric Zell <EricJZell@gmail.com>
2016-01-20 22:36:07 -0500

    Add some about and contact controller actions and properties

--- app/controllers/about.js
+++ app/controllers/about.js
@@ -1,9 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  isPictureShowing: false,
   actions: {
     showRealName: function(){
       alert("Dracula is fictional, but was inspired by 15th century Romanian general and Wallachian Prince Vlad III the Impaler");
+    },
+    showPicture: function(){
+      this.set('isPictureShowing', true)
+    },
+    hidePicture: function(){
+      this.set('isPictureShowing', false)
     }
   }
 });
